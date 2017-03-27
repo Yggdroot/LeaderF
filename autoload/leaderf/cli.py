Yggdroot/LeaderF

commit 47d340af77173ca866c6da56d3762726709be836

fix issue #41: High cup usage.

autoload/leaderf/cli.py
@@ -291,6 +291,7 @@ class LfCli(object):
                 self._buildPrompt()
                 self._idle = False
 
+                lfCmd("sleep 1m")
                 lfCmd("let nr = getchar(1)")
                 if lfEval("!type(nr) && nr == 0") == '1':
                     self._idle = True

commit 52ace18d737845266a41d0387c0a2cc28cfcabd5

fix the issue that <CR> can not open file, the issue is caused by 47d340a

autoload/leaderf/cli.py
@@ -3,6 +3,7 @@
 
 import vim
 import re
+import time
 from datetime import datetime
 from functools import wraps
 from leaderf.utils import *
@@ -290,7 +291,7 @@ class LfCli(object):
                 self._buildPrompt()
                 self._idle = False
 
-                lfCmd("sleep 1m")
+                time.sleep(0.001)
                 lfCmd("let nr = getchar(1)")
                 if lfEval("!type(nr) && nr == 0") == '1':
                     self._idle = True