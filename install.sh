419b7f71e05da1aa89a1f12bef86f55f02fb04cf
aceforeverd <dace1p@gmail.com>
2017-10-05 22:55:45 +0800

    fix installing C extensions on distros map python to python3. #69

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+cd "$(dirname "$0")"
+PYTHON2_EXEC=$(command -v python2)
+PYTHON3_EXEC=$(command -v python3)
+
 if [ "$1" == "--reverse" ]
 then
     cd autoload/leaderf/fuzzyMatch_C
@@ -10,27 +14,32 @@ then
 fi
 
 cd autoload/leaderf/fuzzyMatch_C
-echo "Begin to compile C extension of Python2 ..."
-python setup.py build
-if (( $? == 0 ))
-then
-    cp build/lib*2.?/fuzzyMatchC*.so ../python
+
+if [ -x "$PYTHON2_EXEC" ] ; then
+    echo "Begin to compile C extension of Python2 ..."
+    $PYTHON2_EXEC setup.py build
     if (( $? == 0 ))
     then
-        echo
-        echo C extension of Python2 installed sucessfully!
+        cp build/lib*2.?/fuzzyMatchC*.so ../python
+        if (( $? == 0 ))
+        then
+            echo
+            echo C extension of Python2 installed sucessfully!
+        fi
     fi
 fi
 
-echo
-echo "Begin to compile C extension of Python3 ..."
-python3 setup.py build
-if (( $? == 0 ))
-then
-    cp build/lib*3.?/fuzzyMatchC*.so ../python
+if [ -x "$PYTHON3_EXEC" ] ; then
+    echo
+    echo "Begin to compile C extension of Python3 ..."
+    $PYTHON3_EXEC setup.py build
     if (( $? == 0 ))
     then
-        echo
-        echo C extension of Python3 installed sucessfully!
+        cp build/lib*3.?/fuzzyMatchC*.so ../python
+        if (( $? == 0 ))
+        then
+            echo
+            echo C extension of Python3 installed sucessfully!
+        fi
     fi
 fi
