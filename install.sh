9deeb10e6ac4b90d11fce9dbb85aabb3f69a00f9
Arseny Nasokin <485399+eirnym@users.noreply.github.com>
2018-09-12 23:47:44 +0200

    Add checks for various environments
    Env1: Python2 isn't installed
    Env2: Python3 isn't installed
    Env3: no python is installed

--- install.sh
+++ install.sh
@@ -9,28 +9,43 @@ then
     exit 0
 fi
 
+no_python=true
+
 cd autoload/leaderf/fuzzyMatch_C
-echo "Begin to compile C extension of Python2 ..."
-python2 setup.py build
-if [ $? -eq 0 ]
-then
-    cp build/lib*2.?/fuzzyMatchC*.so ../python
-    if [ $? -eq 0 ]
-    then
-        echo
-        echo C extension of Python2 installed sucessfully!
-    fi
+
+if command -v python2 > /dev/null 2>&1; then
+  no_python=false
+  echo "Begin to compile C extension of Python2 ..."
+  python2 setup.py build
+  if [ $? -eq 0 ]
+  then
+      cp build/lib*2.?/fuzzyMatchC*.so ../python
+      if [ $? -eq 0 ]
+      then
+          echo
+          echo C extension of Python2 installed sucessfully!
+      fi
+  fi
 fi
 
-echo
-echo "Begin to compile C extension of Python3 ..."
-python3 setup.py build
-if [ $? -eq 0 ]
-then
-    cp build/lib*3.?/fuzzyMatchC*.so ../python
-    if [ $? -eq 0 ]
-    then
-        echo
-        echo C extension of Python3 installed sucessfully!
-    fi
+if command -v python3 > /dev/null 2>&1; then
+  $no_python || echo
+  no_python=false
+  
+  echo "Begin to compile C extension of Python3 ..."
+  python3 setup.py build
+  if [ $? -eq 0 ]
+  then
+      cp build/lib*3.?/fuzzyMatchC*.so ../python
+      if [ $? -eq 0 ]
+      then
+          echo
+          echo C extension of Python3 installed sucessfully!
+      fi
+  fi
+fi
+
+if $no_python; then
+  echo "Can't compile C extension, please install Python2 or Python3" >&2
+  exit 1
 fi
