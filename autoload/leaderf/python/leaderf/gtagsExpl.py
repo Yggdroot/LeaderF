32e92a1b019ff1f681e134da978cecf81d6b49fe
Yggdroot <archofortune@gmail.com>
2019-09-19 09:52:30 +0800

    fix issue #361

--- autoload/leaderf/python/leaderf/gtagsExpl.py
+++ autoload/leaderf/python/leaderf/gtagsExpl.py
@@ -432,7 +432,11 @@ class GtagsExplorer(Explorer):
 
         root, dbpath, exists = self._root_dbpath(filename)
         if not filename.startswith(root):
-            lfCmd("let g:Lf_Debug_Gtags = '%s'" % escQuote(str((filename, root))))
+            if self._has_nvim:
+                vim.async_call(lfCmd, "let g:Lf_Debug_Gtags = '%s'" % escQuote(str((filename, root))))
+            else:
+                lfCmd("let g:Lf_Debug_Gtags = '%s'" % escQuote(str((filename, root))))
+
             return
 
         self._updateLibGtags(dbpath)
