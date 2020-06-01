Yggdroot/LeaderF

commit c3795e16a5d972df7e02c6a4dff6c47c24a8ff21

fix issue #608

autoload/leaderf/python/leaderf/bufExpl.py
@@ -135,7 +135,7 @@ class BufExplManager(Manager):
             buf_name = lfEval("bufname(%s)" % buf_number)
             lfCmd("tab drop %s" % escSpecial(buf_name))
         else:
-            if lfEval("get(g:, 'Lf_JumpToExistingWindow', 0)") == '1':
+            if lfEval("get(g:, 'Lf_JumpToExistingWindow', 1)") == '1':
                 buf_name = lfEval("bufname(%s)" % buf_number)
                 lfCmd("hide drop %s" % escSpecial(buf_name))
             else:

autoload/leaderf/python/leaderf/fileExpl.py
@@ -830,7 +830,7 @@ class FileExplManager(Manager):
                     and len(vim.tabpages) == 1 and len(vim.current.tabpage.windows) == 1
                     and vim.current.buffer.name == '' and len(vim.current.buffer) == 1
                     and vim.current.buffer[0] == '' and not vim.current.buffer.options["modified"]):
-                if lfEval("get(g:, 'Lf_JumpToExistingWindow', 0)") == '1':
+                if lfEval("get(g:, 'Lf_JumpToExistingWindow', 1)") == '1':
                     lfCmd("hide drop %s" % escSpecial(file))
                 else:
                     if vim.current.buffer.options["modified"]:

autoload/leaderf/python/leaderf/gtagsExpl.py
@@ -911,7 +911,7 @@ class GtagsExplManager(Manager):
             if kwargs.get("mode", '') == 't':
                 lfCmd("tab drop %s | %s" % (escSpecial(file), line_num))
             else:
-                if lfEval("get(g:, 'Lf_JumpToExistingWindow', 0)") == '1':
+                if lfEval("get(g:, 'Lf_JumpToExistingWindow', 1)") == '1':
                     lfCmd("hide drop %s | %s" % (escSpecial(file), line_num))
                 else:
                     lfCmd("hide edit +%s %s" % (line_num, escSpecial(file)))

autoload/leaderf/python/leaderf/mruExpl.py
@@ -148,7 +148,7 @@ class MruExplManager(Manager):
             if kwargs.get("mode", '') == 't':
                 lfCmd("tab drop %s" % escSpecial(file))
             else:
-                if lfEval("get(g:, 'Lf_JumpToExistingWindow', 0)") == '1':
+                if lfEval("get(g:, 'Lf_JumpToExistingWindow', 1)") == '1':
                     lfCmd("hide drop %s" % escSpecial(file))
                 else:
                     lfCmd("hide edit %s" % escSpecial(file))

autoload/leaderf/python/leaderf/rgExpl.py
@@ -444,7 +444,7 @@ class RgExplManager(Manager):
                 if kwargs.get("mode", '') == 't':
                     lfCmd("tab drop %s | %s" % (escSpecial(file), line_num))
                 else:
-                    if lfEval("get(g:, 'Lf_JumpToExistingWindow', 0)") == '1':
+                    if lfEval("get(g:, 'Lf_JumpToExistingWindow', 1)") == '1':
                         lfCmd("hide drop %s | %s" % (escSpecial(file), line_num))
                     else:
                         lfCmd("hide edit +%s %s" % (line_num, escSpecial(file)))

autoload/leaderf/python/leaderf/tagExpl.py
@@ -80,7 +80,7 @@ class TagExplManager(Manager):
             if kwargs.get("mode", '') == 't':
                 lfCmd("tab drop %s" % escSpecial(tagfile))
             else:
-                if lfEval("get(g:, 'Lf_JumpToExistingWindow', 0)") == '1':
+                if lfEval("get(g:, 'Lf_JumpToExistingWindow', 1)") == '1':
                     lfCmd("hide drop %s" % escSpecial(tagfile))
                 else:
                     lfCmd("hide edit %s" % escSpecial(tagfile))

commit 481978c66e8ba759c47c0d73562126bbffb3f561

c-o is broken by c3795e1

autoload/leaderf/python/leaderf/bufExpl.py
@@ -137,7 +137,7 @@ class BufExplManager(Manager):
         else:
             if lfEval("get(g:, 'Lf_JumpToExistingWindow', 1)") == '1':
                 buf_name = lfEval("bufname(%s)" % buf_number)
-                lfCmd("hide drop %s" % escSpecial(buf_name))
+                lfCmd("keepj hide drop %s" % escSpecial(buf_name))
             else:
                 lfCmd("hide buffer %d" % buf_number)
 

autoload/leaderf/python/leaderf/fileExpl.py
@@ -831,7 +831,7 @@ class FileExplManager(Manager):
                     and vim.current.buffer.name == '' and len(vim.current.buffer) == 1
                     and vim.current.buffer[0] == '' and not vim.current.buffer.options["modified"]):
                 if lfEval("get(g:, 'Lf_JumpToExistingWindow', 1)") == '1':
-                    lfCmd("hide drop %s" % escSpecial(file))
+                    lfCmd("keepj hide drop %s" % escSpecial(file))
                 else:
                     if vim.current.buffer.options["modified"]:
                         lfCmd("hide edit %s" % escSpecial(file))

autoload/leaderf/python/leaderf/gtagsExpl.py
@@ -912,7 +912,7 @@ class GtagsExplManager(Manager):
                 lfCmd("tab drop %s | %s" % (escSpecial(file), line_num))
             else:
                 if lfEval("get(g:, 'Lf_JumpToExistingWindow', 1)") == '1':
-                    lfCmd("hide drop %s | %s" % (escSpecial(file), line_num))
+                    lfCmd("keepj hide drop %s | %s" % (escSpecial(file), line_num))
                 else:
                     lfCmd("hide edit +%s %s" % (line_num, escSpecial(file)))
             lfCmd("norm! ^zv")

autoload/leaderf/python/leaderf/mruExpl.py
@@ -149,7 +149,7 @@ class MruExplManager(Manager):
                 lfCmd("tab drop %s" % escSpecial(file))
             else:
                 if lfEval("get(g:, 'Lf_JumpToExistingWindow', 1)") == '1':
-                    lfCmd("hide drop %s" % escSpecial(file))
+                    lfCmd("keepj hide drop %s" % escSpecial(file))
                 else:
                     lfCmd("hide edit %s" % escSpecial(file))
         except vim.error as e: # E37

autoload/leaderf/python/leaderf/rgExpl.py
@@ -445,7 +445,7 @@ class RgExplManager(Manager):
                     lfCmd("tab drop %s | %s" % (escSpecial(file), line_num))
                 else:
                     if lfEval("get(g:, 'Lf_JumpToExistingWindow', 1)") == '1':
-                        lfCmd("hide drop %s | %s" % (escSpecial(file), line_num))
+                        lfCmd("keepj hide drop %s | %s" % (escSpecial(file), line_num))
                     else:
                         lfCmd("hide edit +%s %s" % (line_num, escSpecial(file)))
             else:

autoload/leaderf/python/leaderf/tagExpl.py
@@ -81,7 +81,7 @@ class TagExplManager(Manager):
                 lfCmd("tab drop %s" % escSpecial(tagfile))
             else:
                 if lfEval("get(g:, 'Lf_JumpToExistingWindow', 1)") == '1':
-                    lfCmd("hide drop %s" % escSpecial(tagfile))
+                    lfCmd("keepj hide drop %s" % escSpecial(tagfile))
                 else:
                     lfCmd("hide edit %s" % escSpecial(tagfile))
         except vim.error as e: # E37