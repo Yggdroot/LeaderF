Yggdroot/LeaderF

commit 4efc9f6d36f92927f206eca60f14f1188fb76603

I use vim-gutentags to generate gtags, so i need compatibility (#687)
* compatible with https://github.com/ludovicchabant/vim-gutentags
Co-authored-by: 张旭 <zvn6761@corp.netease.com>

autoload/leaderf/python/leaderf/gtagsExpl.py
@@ -395,10 +395,15 @@ class GtagsExplorer(Explorer):
             return False
 
     def _generateDbpath(self, path):
+        with_gutentags = lfEval("get(g:, 'Lf_GtagsGutentags', 0)") != '0'
+        sep_char = '-' if with_gutentags else '_'
         if os.name == 'nt':
-            db_folder = re.sub(r'[\\/]', '_', path.replace(':\\', '_', 1))
+            if with_gutentags:
+                db_folder = re.sub(r'[:\\/]', sep_char, path)
+            else:
+                db_folder = re.sub(r'[\\/]', sep_char, path.replace(':\\', sep_char, 1))
         else:
-            db_folder = path.replace('/', '_')
+            db_folder = path.replace('/', sep_char)
 
         if self._store_in_project:
             return path

commit 7181bfc19910e55d26787429bb5d36ee8674b37b

fix a bug introduced by #687 Request from non-main thread

autoload/leaderf/python/leaderf/gtagsExpl.py
@@ -43,6 +43,7 @@ class GtagsExplorer(Explorer):
         self._db_timestamp = 0
         self._last_command = ""
         self._content = []
+        self._with_gutentags = lfEval("get(g:, 'Lf_GtagsGutentags', 0)") != '0'
 
         self._task_queue = Queue.Queue()
         self._worker_thread = threading.Thread(target=self._processTask)
@@ -395,10 +396,9 @@ class GtagsExplorer(Explorer):
             return False
 
     def _generateDbpath(self, path):
-        with_gutentags = lfEval("get(g:, 'Lf_GtagsGutentags', 0)") != '0'
-        sep_char = '-' if with_gutentags else '_'
+        sep_char = '-' if self._with_gutentags else '_'
         if os.name == 'nt':
-            if with_gutentags:
+            if self._with_gutentags:
                 db_folder = re.sub(r'[:\\/]', sep_char, path)
             else:
                 db_folder = re.sub(r'[\\/]', sep_char, path.replace(':\\', sep_char, 1))