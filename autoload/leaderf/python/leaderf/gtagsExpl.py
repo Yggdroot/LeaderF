Yggdroot/LeaderF

commit f25fd1729b0dc80ed960d6e0025210e30e855d8f

LeaderfTag should also take consider of Lf_JumpToExistingWindow (#644)
Actually, this is the real solution for #643.

Signed-off-by: Hao Lee <haolee.swjtu@gmail.com>

autoload/leaderf/python/leaderf/gtagsExpl.py
@@ -910,7 +910,10 @@ class GtagsExplManager(Manager):
 
         try:
             if kwargs.get("mode", '') == 't':
-                lfCmd("tab drop %s | %s" % (escSpecial(file), line_num))
+                if lfEval("get(g:, 'Lf_JumpToExistingWindow', 1)") == '1':
+                    lfCmd("tab drop %s" % (escSpecial(file), line_num))
+                else:
+                    lfCmd("tabe %s" % (escSpecial(file), line_num))
             else:
                 if lfEval("get(g:, 'Lf_JumpToExistingWindow', 1)") == '1' and lfEval("bufexists('%s')" % escQuote(file)) == '1':
                     lfCmd("keepj hide drop %s | %s" % (escSpecial(file), line_num))

autoload/leaderf/python/leaderf/tagExpl.py
@@ -78,7 +78,10 @@ class TagExplManager(Manager):
         tagaddress = res[0]
         try:
             if kwargs.get("mode", '') == 't':
-                lfCmd("tab drop %s" % escSpecial(tagfile))
+                if lfEval("get(g:, 'Lf_JumpToExistingWindow', 1)") == '1':
+                    lfCmd("tab drop %s" % escSpecial(tagfile))
+                else:
+                    lfCmd("tabe %s" % escSpecial(tagfile))
             else:
                 if lfEval("get(g:, 'Lf_JumpToExistingWindow', 1)") == '1' and lfEval("bufexists('%s')" % escQuote(tagfile)) == '1':
                     lfCmd("keepj hide drop %s" % escSpecial(tagfile))

commit 1cbf293bfdcfa9e8e3befb63842d31597630871d

fix a bug introduced by #644

autoload/leaderf/python/leaderf/gtagsExpl.py
@@ -919,9 +919,9 @@ class GtagsExplManager(Manager):
         try:
             if kwargs.get("mode", '') == 't':
                 if lfEval("get(g:, 'Lf_JumpToExistingWindow', 1)") == '1':
-                    lfCmd("tab drop %s" % (escSpecial(file), line_num))
+                    lfCmd("tab drop %s | %s" % (escSpecial(file), line_num))
                 else:
-                    lfCmd("tabe %s" % (escSpecial(file), line_num))
+                    lfCmd("tabe %s | %s" % (escSpecial(file), line_num))
             else:
                 if lfEval("get(g:, 'Lf_JumpToExistingWindow', 1)") == '1' and lfEval("bufexists('%s')" % escQuote(file)) == '1':
                     lfCmd("keepj hide drop %s | %s" % (escSpecial(file), line_num))