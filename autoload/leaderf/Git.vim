Yggdroot/LeaderF

commit 570b2f62b1c3d2ace893d203f970c6da3e619c71

refactor the code

autoload/leaderf/Git.vim
@@ -400,28 +400,28 @@ function! leaderf#Git#TreeViewMaps(id) abort
     nnoremap <buffer> <silent> )             :call leaderf#Git#OuterBlock(1)<CR>
 endfunction
 
-function! leaderf#Git#CollapseParent(explorer_page) abort
+function! leaderf#Git#CollapseParent(navigation_panel) abort
     if leaderf#Git#OuterIndent(0) != 0
-        exec g:Lf_py printf("%s.open(False)", a:explorer_page)
+        exec g:Lf_py printf("%s.openDiffView(False)", a:navigation_panel)
     endif
 endfunction
 
-function! leaderf#Git#ExplorerMaps(id) abort
+function! leaderf#Git#NavigationPanelMaps(id) abort
     exec g:Lf_py "import ctypes"
-    let explorer_page = printf("ctypes.cast(%d, ctypes.py_object).value", a:id)
-    exec printf('nnoremap <buffer> <silent> o             :exec g:Lf_py "%s.open(False)"<CR>', explorer_page)
-    exec printf('nnoremap <buffer> <silent> <2-LeftMouse> :exec g:Lf_py "%s.open(False)"<CR>', explorer_page)
-    exec printf('nnoremap <buffer> <silent> <CR>          :exec g:Lf_py "%s.open(False)"<CR>', explorer_page)
-    exec printf('nnoremap <buffer> <silent> O             :exec g:Lf_py "%s.open(True)"<CR>', explorer_page)
-    exec printf('nnoremap <buffer> <silent> t             :exec g:Lf_py "%s.open(False, mode=''t'')"<CR>', explorer_page)
-    exec printf('nnoremap <buffer> <silent> s             :exec g:Lf_py "%s.toggleDiffViewMode()"<CR>', explorer_page)
-    exec printf('nnoremap <buffer> <silent> i             :exec g:Lf_py "%s.toggleIgnoreWhitespace()"<CR>', explorer_page)
-    exec printf('nnoremap <buffer> <silent> p             :exec g:Lf_py "%s.open(False, preview=True)"<CR>', explorer_page)
-    exec printf('nnoremap <buffer> <silent> x             :call leaderf#Git#CollapseParent("%s")<CR>', explorer_page)
-    exec printf('nnoremap <buffer> <silent> f             :exec g:Lf_py "%s.fuzzySearch()"<CR>', explorer_page)
-    exec printf('nnoremap <buffer> <silent> F             :exec g:Lf_py "%s.fuzzySearch(True)"<CR>', explorer_page)
-    exec printf('nnoremap <buffer> <silent> m             :exec g:Lf_py "%s.showCommitMessage()"<CR>', explorer_page)
-    exec printf('nnoremap <buffer> <silent> <LeftRelease> <LeftRelease>:exec g:Lf_py "%s.selectOption()"<CR>', explorer_page)
+    let navigation_panel = printf("ctypes.cast(%d, ctypes.py_object).value", a:id)
+    exec printf('nnoremap <buffer> <silent> o             :exec g:Lf_py "%s.openDiffView(False)"<CR>', navigation_panel)
+    exec printf('nnoremap <buffer> <silent> <2-LeftMouse> :exec g:Lf_py "%s.openDiffView(False)"<CR>', navigation_panel)
+    exec printf('nnoremap <buffer> <silent> <CR>          :exec g:Lf_py "%s.openDiffView(False)"<CR>', navigation_panel)
+    exec printf('nnoremap <buffer> <silent> O             :exec g:Lf_py "%s.openDiffView(True)"<CR>', navigation_panel)
+    exec printf('nnoremap <buffer> <silent> t             :exec g:Lf_py "%s.openDiffView(False, mode=''t'')"<CR>', navigation_panel)
+    exec printf('nnoremap <buffer> <silent> s             :exec g:Lf_py "%s.toggleDiffViewMode()"<CR>', navigation_panel)
+    exec printf('nnoremap <buffer> <silent> i             :exec g:Lf_py "%s.toggleIgnoreWhitespace()"<CR>', navigation_panel)
+    exec printf('nnoremap <buffer> <silent> p             :exec g:Lf_py "%s.openDiffView(False, preview=True)"<CR>', navigation_panel)
+    exec printf('nnoremap <buffer> <silent> x             :call leaderf#Git#CollapseParent("%s")<CR>', navigation_panel)
+    exec printf('nnoremap <buffer> <silent> f             :exec g:Lf_py "%s.fuzzySearch()"<CR>', navigation_panel)
+    exec printf('nnoremap <buffer> <silent> F             :exec g:Lf_py "%s.fuzzySearch(True)"<CR>', navigation_panel)
+    exec printf('nnoremap <buffer> <silent> m             :exec g:Lf_py "%s.showCommitMessage()"<CR>', navigation_panel)
+    exec printf('nnoremap <buffer> <silent> <LeftRelease> <LeftRelease>:exec g:Lf_py "%s.selectOption()"<CR>', navigation_panel)
     nnoremap <buffer> <silent> q             :q<CR>
 endfunction
 

commit 5316ba1976c7bdcc0033d0f5f9d434cd4dceaf5e

`--side-by-side` is broken by 570b2f6

autoload/leaderf/Git.vim
@@ -542,7 +542,7 @@ function! leaderf#Git#Commands() abort
                     \ {"Leaderf git diff --side-by-side":          "fuzzy search and view the side-by-side diffs"},
                     \ {"Leaderf git diff --side-by-side --current-file":"view the side-by-side diffs of the current file"},
                     \ {"Leaderf git diff --explorer":              "view the diffs in an explorer tabpage"},
-                    \ {"Leaderf git diff --explorer --unified":    "view the diffs in an explorer tabpage, the diffs is shown in unified view"},
+                    \ {"Leaderf git diff --explorer --side-by-side":"view the diffs in an explorer tabpage, the diffs is shown in a side-by-side view"},
                     \ {"Leaderf git diff --directly":              "view the diffs directly"},
                     \ {"Leaderf git diff --directly --position right":"view the diffs in the right split window"},
                     \ {"Leaderf git diff --cached":                "fuzzy search and view `git diff --cached`"},
@@ -558,7 +558,7 @@ function! leaderf#Git#Commands() abort
                     \ {"Leaderf git log":                          "fuzzy search and view the log"},
                     \ {"Leaderf git log --directly":               "view the logs directly"},
                     \ {"Leaderf git log --explorer":               "fuzzy search and view the log in an explorer tabpage"},
-                    \ {"Leaderf git log --explorer --unified":     "fuzzy search and view the log in an explorer tabpage, the diffs is shown in unified view"},
+                    \ {"Leaderf git log --explorer --side-by-side":"fuzzy search and view the log in an explorer tabpage, the diffs is shown in a side-by-side view"},
                     \ {"Leaderf git log --explorer --navigation-position bottom": "specify the position of navigation panel in explorer tabpage"},
                     \ {"Leaderf git log --current-file":           "fuzzy search and view the log of current file"},
                     \ {"Leaderf git log --current-file --explorer":"fuzzy search and view the log of current file in explorer tabpage"},

autoload/leaderf/python/leaderf/gitExpl.py
@@ -3339,7 +3339,11 @@ class ExplorerPage(object):
             buffer_name = command.getBufferName()
             winid = self._createWindow(win_pos, buffer_name)
 
-        callback = partial(self.getDiffViewPanel().create,
+        def createDiffViewPanel(get_diff_view_panel, arguments_dict, source, **kwargs):
+            return get_diff_view_panel().create(arguments_dict, source, **kwargs)
+
+        callback = partial(createDiffViewPanel,
+                           self.getDiffViewPanel,
                            arguments_dict,
                            winid=diff_view_winid,
                            line_num=line_num,